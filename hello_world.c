220cac7bc7bd6676c1073cdcfff3560a55e97f0b
Kanchain <1683017738@qq.com>
2025-06-26 10:26:37 +0800

    second save

--- hello_world.c
+++ hello_world.c
@@ -1,5 +1,8 @@
 #include "stdio.h"
 int main() {
     printf("Hello, World!\n");
+    printf("This is a C program using the C11 standard.\n");
+    printf("The C11 standard introduces several new features and improvements.\n");
+    printf("For example, it includes support for multi-threading and improved type-generic macros.\n"); 
     return 0;
 }
