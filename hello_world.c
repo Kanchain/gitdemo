abc1db744d6c2898f4a253685d477a61728d85b3
Kanchain <1683017738@qq.com>
2025-06-26 13:18:37 +0800

    c4

--- hello_world.c
+++ hello_world.c
@@ -3,5 +3,9 @@ int main() {
     printf("Hello, World!\n");
     printf("This is a C program using the C11 standard.\n");
     printf("The C11 standard introduces several new features and improvements.\n");
+    int a = 5;
+    int b = 10;
+    int sum = a + b;
+    printf("The sum of %d and %d is %d.\n", a, b, sum);
     return 0;
 }
